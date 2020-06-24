4a694c3c970e1373b02052d532038ca75850bb8a
Ronan Finley <17411952+RonanFinley@users.noreply.github.com>
2020-06-23 22:22:31 -0600

    Add content from Stonework Rose
    Content of the Emissary of the Flock in Stonework Rose will be removed in favour of this mod

---
+++ platinumWorld.cs
@@ -0,0 +1,27 @@
+﻿using Platinum.NPCs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace Platinum {
+    class platinumWorld : ModWorld {
+		public override void Initialize() {
+		}
+		public override TagCompound Save() {
+
+			return new TagCompound {
+				["emissary"] = EmissaryOfTheFlock.Save()
+			};
+		}
+		public override void Load(TagCompound tag) {
+			EmissaryOfTheFlock.Load(tag.GetCompound("emissary"));
+		}
+		public override void PreUpdate() {
+			EmissaryOfTheFlock.UpdateMerchandise();
+		}
+	}
+}
